8057f3ff50d489d26f9c4499db6d312da9922783
Sam Gillet-Garas <sgilletgaras@gmail.com>
2022-06-03 20:49:21 +0200

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div id="header_container">
+        <h1>Samantha Gillet-Garas</h1>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
