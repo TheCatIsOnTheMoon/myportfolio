9a80fdce96c31ee452814f5a121842fdbd1438ba
Sam Gillet-Garas <sgilletgaras@gmail.com>
2022-06-05 11:07:35 +0200

    set up tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from './layout/Home/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Given I am on the Home page', () => {
+  test('Then, it should render the about section', () => {
+    render(<Home />);
+    const aboutSection = screen.getByText(/Front-End Developer/i);
+    expect(aboutSection).toBeInTheDocument();
+  });
+
+  test('Then, it should render the skills section', () => {
+    render(<Home />);
+    const aboutSection = screen.getByText(/Skills and tech/i);
+    expect(aboutSection).toBeInTheDocument();
+  });
+
+  test('Then, it should render the projects section', () => {
+    render(<Home />);
+    const aboutSection = screen.getByText(/projects/i);
+    expect(aboutSection).toBeInTheDocument();
+  });
+
+  test('Then, it should render the contact section', () => {
+    render(<Home />);
+    const aboutSection = screen.getByText(/contact/i);
+    expect(aboutSection).toBeInTheDocument();
+  });
 });
