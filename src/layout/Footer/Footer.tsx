6bd7e01175d485bd83c6dd987376d9dc3edd63fb
Sam Gillet-Garas <sgilletgaras@gmail.com>
2022-06-06 15:06:11 +0200

    footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <footer>
       <p>Website from scratch, with love, by me</p>
-      <p>Icons from https://devicon.dev/</p>
+      <p>-</p>
+      <p>Deployed on <a href="https://www.netlify.com/">Netlify</a></p>
+      <p>Icons from <a href="https://devicon.dev/">Devicon</a></p>
     </footer>
   );
 }
