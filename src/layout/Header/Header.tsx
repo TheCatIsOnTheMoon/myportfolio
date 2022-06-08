6551f791d953f69f288a118818a7dd27562b4c1a
Sam Gillet-Garas <sgilletgaras@gmail.com>
2022-06-08 16:09:22 +0200

    new design tweaks

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -9,11 +9,11 @@ function Header() {
         <div className="header__container">
           <h1>Samantha Gillet-Garas</h1>
           <div className="header__container__links">
-            <a href="https://www.linkedin.com/in/gilletgaras-samantha/">
-              <img className="header__container__links__icon" src={iconLinkedin} alt="link linkedIn" />
-            </a>
             <a href="https://github.com/TheCatIsOnTheMoon">
               <img className="header__container__links__icon" src={iconGithub} alt="link github" />
+            </a>
+            <a href="https://www.linkedin.com/in/gilletgaras-samantha/">
+              <img className="header__container__links__icon" src={iconLinkedin} alt="link linkedIn" />
             </a>
           </div>
         </div>
